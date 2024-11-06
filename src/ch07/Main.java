f69e30f909866637e5982e1502bb9efade414c11
GilMinSeon <littor1886@gmail.com>
2024-11-06 23:00:29 +0900

    7장 배열 - 8번. 빗물트래핑 #2 스택 풀이 정리 완료

--- src/ch07/Main.java
+++ src/ch07/Main.java
@@ -7,86 +7,56 @@ import java.sql.Array;
 import java.util.*;
 import java.util.stream.*;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
 // BOJ 18428 감시피하기
 public class Main {
-    static String[][] maze;
-    static int N;
-    static int[] dir_x = {-1, 1, 0, 0};
-    static int[] dir_y = {0, 0, 1, -1};
-    static ArrayList<int[]> teacher = new ArrayList<>();
-    static ArrayList<int[]> empty = new ArrayList<>();
-    static String answer = "NO";
 
-    public static boolean isOK(int a, int b) {
-        return (0<=a && a <N && 0<=b && b<N);
+    public static void main(String[] args) {
+        //int res = trap(new int[]{3, 1, 1, 1, 4});
+        int res = trap(new int[]{4,3,2,1,5});
+        System.out.println(res);
     }
 
-    public static void blocking(int start, int depth, ArrayList<int[]> spots) {
-        if(answer.equals("YES"))
-            return;
+    public static int trap(int[] height) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int volume = 0;
 
-        if(depth == 3) {
-            String[][] tmp = new String[N][N];
+        for (int i = 0; i < height.length; i++) {
+            // 변곡점을 만나는 경우
+            System.out.println("i >>>>" + i);
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                System.out.println("스택 진입");
+                // 스택에서 꺼낸다.
+                Integer top = stack.pop();
 
-            for(int i = 0; i < N; i++)
-                tmp[i] = Arrays.copyOf(maze[i], N);
+                if (stack.isEmpty())
+                    break;
 
-            for(int i=0; i<3; i++)
-                tmp[spots.get(i)[0]][spots.get(i)[1]] = "O";
+                // 스택의 마지막 위치까지를 거리로 계산
+                int distance = i - stack.peek() - 1;
+                System.out.println(stack.peek() + "      !!!!");
+                // 현재 높이 또는 스택의 마지막 위치 높이 중 낮은 값에 방금 꺼낸 높이의 차이를 물 높이로 지정
+                int waters = Math.min(height[i], height[stack.peek()]) - height[top];
 
-            int found = 0;
-            for(int[] location : teacher) {
-                for(int j=0; j<4; j++) {
-                    int dx = location[0] + dir_x[j];
-                    int dy = location[1] + dir_y[j];
-                    found += DFS(dx, dy, tmp, j);
-                }
+                System.out.println("waters >>>>" + waters + "    distance :::" + distance);
+                // 물이 쌓이는 양은 거리와 물 높이의 곱
+                volume += distance * waters;
+                System.out.println("vol :::" + volume);
+
             }
-            answer = (found == 0) ? "YES" : "NO";
-            return;
+
+            // 진행하면서 현재 위치를 스택에 삽입
+            stack.push(i);
+            System.out.println("stack >>" + stack);
+            System.out.println(i + "    for문 end");
+            System.out.println();
+
         }
-
-        for(int i=start; i<empty.size(); i++) {
-            int[] arr = empty.get(i);
-            if (maze[arr[0]][arr[1]].equals("X")) {
-                spots.add(new int[]{arr[0], arr[1]});
-                blocking(i+1, depth + 1, spots);
-                spots.remove(spots.size()-1);
-            }
-        }
-    }
-
-    public static int DFS(int x, int y, String[][] maps, int dir) {
-        if(!isOK(x, y) || maps[x][y].equals("O"))
-            return 0;
-
-        if(maps[x][y].equals("S"))
-            return 1;
-
-        return DFS(x+dir_x[dir], y+dir_y[dir], maps, dir);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        maze = new String[N][N];
-
-        for(int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
-                String str = st.nextToken();
-                maze[i][j] = str;
-
-                if(str.equals("T"))
-                    teacher.add(new int[]{i, j});
-                if(str.equals("X"))
-                    empty.add(new int[]{i, j});
-            }
-        }
-
-        blocking(0, 0, new ArrayList<>());
-
-        System.out.println(answer);
+        return volume;
     }
 }
+
 
